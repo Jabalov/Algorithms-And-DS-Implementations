5e0a900ec87b8244d0c7c3a5f0d8413e202e200e
Jabalov <moh.aboajabl@gmail.com>
2019-05-07 13:50:00 +0200

    . . .

--- UCSD/C1/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.cpp
+++ UCSD/C1/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cassert>
+using namespace std;
 
 // The following code calls a naive algorithm for computing a Fibonacci number.
 //
@@ -14,32 +15,39 @@
 // 5. Remove the call to test_solution, uncomment the line with a call to fibonacci_fast (and the lines reading the input),
 //    and submit it to the grader.
 
-int fibonacci_naive(int n) {
+long long fibonacci_naive(long long n)
+{
     if (n <= 1)
         return n;
 
     return fibonacci_naive(n - 1) + fibonacci_naive(n - 2);
 }
 
-int fibonacci_fast(int n) {
-    // write your code here
-
-    return 0;
+long long fib[46];
+long long fibonacci_fast(long long n)
+{
+    return fib[n];
 }
 
-void test_solution() {
+void test_solution()
+{
     assert(fibonacci_fast(3) == 2);
     assert(fibonacci_fast(10) == 55);
-    for (int n = 0; n < 20; ++n)
+    for (long long n = 0; n < 20; ++n)
         assert(fibonacci_fast(n) == fibonacci_naive(n));
 }
 
-int main() {
-    int n = 0;
-    std::cin >> n;
+int main()
+{
+    long long n = 0;
+    cin >> n;
 
-    std::cout << fibonacci_naive(n) << '\n';
-    //test_solution();
-    //std::cout << fibonacci_fast(n) << '\n';
+    fib[0] = 0, fib[1] = 1;
+    for (long long i = 2; i < 46; i++)
+        fib[i] = fib[i - 1] + fib[i - 2];
+
+    //cout << fibonacci_naive(n) << '\n';
+    test_solution();
+    cout << fibonacci_fast(n) << '\n';
     return 0;
 }
