c300bf9ddeb0429f213aa6455bf5e1fa29b8e707
Jabalov <moh.aboajabl@gmail.com>
2019-05-20 23:13:05 +0200

    _Remember Remember The Fifth of November_

--- Sort/selection.cpp
+++ Sort/selection.cpp
@@ -1,2 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+#define ll long long
+
+void selection_sort(ll A[])
+{
+    ll pos = 0, min = 0;
+    ll n = 10;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        pos = i, min = A[i];
+        for (int j = i + 1; j < n; j++)
+        {
+            if (A[j] < A[pos])
+                min = A[j], pos = j;
+        }
+        swap(A[i], A[pos]);
+    }
+}
+
+int main()
+{
+    const int n = 10;
+    ll A[n];
+
+    for (int i = 0; i < 10; i++)
+        cin >> A[i];
+
+    selection_sort(A);
+
+    for (int i = 0; i < 10; i++)
+        cout << A[i] << " ";
+}
