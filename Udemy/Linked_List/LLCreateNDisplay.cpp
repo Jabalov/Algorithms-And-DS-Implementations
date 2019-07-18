eb8729625173f25d345d7354a7833e0b373fdf3e
Jabalov <moh.aboajabl@gmail.com>
2019-07-18 12:17:15 +0200

    addMax

--- Udemy/Linked_List/LLCreateNDisplay.cpp
+++ Udemy/Linked_List/LLCreateNDisplay.cpp
@@ -73,6 +73,31 @@ int Rsum(struct Node *p)
         return Rsum(p->next) + p->data;
 }
 
+int Max(struct Node *p)
+{
+    int max = INT32_MIN;
+    while (p)
+    {
+        if (p->data > max)
+            max = p->data;
+        p = p->next;
+    }
+    return max;
+}
+
+int RMax(struct Node *p)
+{
+    int x = 0;
+
+    if (p == 0)
+        return INT32_MIN;
+    x = RMax(p->next);
+    if (x > p->data)
+        return x;
+    else
+        return p->data;
+}
+
 int main()
 {
     int A[] = {3, 5, 7, 10, 25, 8, 32, 2};
