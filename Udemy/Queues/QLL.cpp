374d06050c0f1a011e08bca1469d05dd86990ecd
Jabalov <moh.aboajabl@gmail.com>
2019-08-02 09:22:18 +0200

    Queue with Linked List

---
+++ Udemy/Queues/QLL.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+
+} *front = NULL, *rear = NULL;
+
+void enqueue(int x)
+{
+    struct Node *tmp = (struct Node *)malloc(sizeof(struct Node));
+
+    if (tmp == NULL)
+        printf("Queue is Full");
+
+    else
+    {
+        tmp->data = x;
+        tmp->next = NULL;
+
+        if (front == NULL)
+            front = rear = tmp;
+
+        else
+            rear->next = tmp, rear = tmp;
+    }
+}
+
+void Display()
+{
+    struct Node *p = front;
+    while (p)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+    printf("\n");
+}
+
+int dequeue()
+{
+    int x = -1;
+    struct Node *tmp;
+
+    if (front == NULL)
+        printf("Queue is Empty\n");
+
+    else
+    {
+        x = front->data;
+        tmp = front;
+        front = front->next;
+        free(tmp);
+    }
+    return x;
+}
+
+int main()
+{
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    enqueue(40);
+    enqueue(50);
+
+    Display();
+
+    printf("%d ", dequeue());
+
+    return 0;
+}
