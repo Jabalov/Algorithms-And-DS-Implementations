f1724930242dcac96bef371a7f860780c2a5f80f
Jabalov <moh.aboajabl@gmail.com>
2019-08-02 08:25:17 +0200

    Array

--- Udemy/Queues/usingArray.cpp
+++ Udemy/Queues/usingArray.cpp
@@ -5,3 +5,52 @@ struct Queue
 {
     int size, front, rear, *Q;
 };
+
+void create(struct Queue *q, int size)
+{
+    q->size = size;
+    q->front = q->rear = -1;
+    q->Q = (int *)malloc(size * sizeof(int));
+}
+
+void enqueue(struct Queue *q, int x)
+{
+    if (q->rear == q->size - 1)
+        printf("Queue is full");
+    else
+        q->rear++, q->Q[q->rear] = x;
+}
+
+int dequeue(struct Queue *q)
+{
+    int x = -1;
+    if (q->front == q->rear)
+        printf("Queue is Empty\n");
+
+    else
+        q->front++, x = q->Q[q->front];
+
+    return x;
+}
+
+void Display(struct Queue q)
+{
+    int i;
+    for (i = q.front + 1; i <= q.rear; i++)
+        printf("%d ", q.Q[i]);
+    printf("\n");
+}
+
+int main()
+{
+    struct Queue q;
+    create(&q, 5);
+
+    enqueue(&q, 10);
+    enqueue(&q, 20);
+    enqueue(&q, 30);
+    Display(q);
+
+    printf("%d ", dequeue(&q));
+    return 0;
+}
