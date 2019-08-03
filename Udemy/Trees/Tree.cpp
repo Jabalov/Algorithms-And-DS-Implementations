ce69add73b4b990aa2ef4507a3d0bcc8b51d6d12
Jabalov <moh.aboajabl@gmail.com>
2019-08-03 17:53:17 +0200

    H

--- Udemy/Trees/Tree.cpp
+++ Udemy/Trees/Tree.cpp
@@ -141,6 +141,23 @@ int count(struct Node *root)
     return 0;
 }
 
+int height(struct Node *p)
+{
+    int x, y;
+    x = y = 0;
+
+    if (p == NULL)
+        return 0;
+
+    x = height(p->lchild);
+    y = height(p->rchild);
+
+    if (x > y)
+        return x + 1;
+    else
+        return y + 1;
+}
+
 int main()
 {
     treeCreate();
