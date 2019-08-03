4f2479a5101115eb095231e287909a7cc449f8a2
Jabalov <moh.aboajabl@gmail.com>
2019-08-03 12:24:18 +0200

    create

---
+++ Udemy/Trees/Tree.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Queue.h"
+
+struct Node *root = NULL;
+
+void treeCreate()
+{
+    struct Node *p, *t;
+    int x;
+    struct Queue q;
+    create(&q, 100);
+    printf("Enter The root value:\n");
+    scanf("%d", &x);
+
+    root = (struct Node *)malloc(sizeof(struct Node));
+    root->data = x;
+    root->lchild = root->rchild = NULL;
+    enqueue(&q, root);
+
+    while (!isEmpty(q))
+    {
+        p = dequeue(&q);
+        printf("enter left child of: %d\n ", p->data);
+        scanf("%d", &x);
+
+        if (x != -1)
+        {
+            t = (struct Node *)malloc(sizeof(struct Node));
+            t->data = x;
+            t->lchild = t->rchild = NULL;
+            p->lchild = t;
+            enqueue(&q, t);
+        }
+
+        printf("enter right child of: %d\n ", p->data);
+        scanf("%d", &x);
+
+        if (x != -1)
+        {
+            t = (struct Node *)malloc(sizeof(struct Node));
+            t->data = x;
+            t->lchild = t->rchild = NULL;
+            p->rchild = t;
+            enqueue(&q, t);
+        }
+    }
+}
