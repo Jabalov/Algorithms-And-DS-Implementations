2ebfd8988a8ad2b9b2d45cf5ff278a3a7a758286
Jabalov <moh.aboajabl@gmail.com>
2019-08-03 14:49:12 +0200

    Stack Added

---
+++ Udemy/Trees/Stack.h
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Stack
+{
+    int size, top, *s;
+};
+
+void create(struct Stack *s, int x)
+{
+    s->size = x;
+
+    s->top = -1;
+    s->s = (int *)malloc(s->size * sizeof(int));
+}
+
+void display(struct Stack s)
+{
+    int i = s.top;
+    for (; i >= 0; i--)
+        printf("%d ", s.s[i]);
+    printf("\n");
+}
+
+void push(struct Stack *s, int x)
+{
+    if (s->top == s->size - 1)
+        printf("Stack overflow\n");
+    else
+        s->top++, s->s[s->top] = x;
+}
+
+int pop(struct Stack *s)
+{
+    int x = -1;
+    if (s->top == -1)
+        printf("Stack Underflow\n");
+    else
+        x = s->s[s->top--];
+
+    return x;
+}
+
+int peek(struct Stack s, int index)
+{
+    int x = -1;
+    if (s.top - index + 1 < 0)
+        printf("Invalid Index \n");
+    x = s.s[s.top - index + 1];
+    return x;
+}
+
+int isEmpty(struct Stack s)
+{
+    return (s.top == -1);
+}
+
+int isFull(struct Stack s)
+{
+    return s.top == s.size - 1;
+}
+
+int stackTop(struct Stack s)
+{
+    if (!isEmpty(s))
+        return s.s[s.top];
+    return -1;
+}
