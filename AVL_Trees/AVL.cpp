50ff5ab3a839297036feeffadee5f77544d66d85
Jabalov <moh.aboajabl@gmail.com>
2019-08-12 21:08:57 +0200

    Rotations Added

--- AVL_Trees/AVL.cpp
+++ AVL_Trees/AVL.cpp
@@ -34,6 +34,7 @@ void Insert(int key)
         root = p;
         return;
     }
+
     while (t != NULL)
     {
         r = t;
@@ -53,4 +54,167 @@ void Insert(int key)
         r->lchild = p;
     else
         r->rchild = p;
+}
+
+struct Node *Search(int key)
+{
+    struct Node *t = root;
+
+    while (t != NULL)
+    {
+        if (key == t->data)
+            return t;
+        else if (key < t->data)
+            t = t->lchild;
+        else
+            t = t->rchild;
+    }
+    return NULL;
+}
+
+struct Node *LLRotation(struct Node *p)
+{
+    int lbf, rbf;
+    struct Node *pl = p->lchild;
+
+    pl->bf = 0;
+    p->lchild = pl->rchild;
+    pl->rchild = p;
+
+    lbf = height(p->lchild) + 1;
+    rbf = height(p->rchild) + 1;
+
+    p->bf = lbf - rbf;
+    if (p == root)
+        root = pl;
+    return pl;
+}
+
+struct Node *LRRotation(struct Node *p)
+{
+    int lbf, rbf;
+    struct Node *pl = p->lchild;
+    struct Node *plr = pl->rchild;
+
+    plr->bf = 0;
+    p->lchild = plr->rchild;
+    pl->rchild = plr->lchild;
+    plr->lchild = pl;
+    plr->rchild = p;
+
+    lbf = height(p->lchild) + 1;
+    rbf = height(p->rchild) + 1;
+    p->bf = lbf - rbf;
+
+    lbf = height(pl->lchild) + 1;
+    rbf = height(pl->rchild) + 1;
+    pl->bf = lbf - rbf;
+
+    if (p == root)
+        root = plr;
+    return plr;
+}
+
+struct Node *RRRotation(struct Node *p)
+{
+    int lbf, rbf;
+    struct Node *pr = p->rchild;
+
+    pr->bf = 0;
+    p->rchild = pr->lchild;
+    pr->lchild = p;
+
+    lbf = height(p->lchild) + 1;
+    rbf = height(p->rchild) + 1;
+    p->bf = lbf - rbf;
+
+    if (p == root)
+        root = pr;
+    return pr;
+}
+
+struct Node *RLRotation(struct Node *p)
+{
+    int lbf, rbf;
+    struct Node *pr = p->rchild;
+    struct Node *prl = pr->lchild;
+
+    prl->bf = 0;
+    p->rchild = prl->lchild;
+    pr->lchild = prl->rchild;
+    prl->rchild = pr;
+    prl->lchild = p;
+
+    lbf = height(p->lchild) + 1;
+    rbf = height(p->rchild) + 1;
+    p->bf = lbf - rbf;
+
+    lbf = height(pr->lchild) + 1;
+    rbf = height(pr->rchild) + 1;
+    pr->bf = lbf - rbf;
+
+    if (p == root)
+        root = prl;
+    return prl;
+}
+
+struct Node *RInsert(struct Node *p, int key)
+{
+    struct Node *t;
+    int lbf, rbf;
+
+    if (p == NULL)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = key;
+        t->bf = 0;
+        t->lchild = t->rchild = NULL;
+        return t;
+    }
+
+    if (key < p->data)
+        p->lchild = RInsert(p->lchild, key);
+
+    else if (key > p->data)
+        p->rchild = RInsert(p->rchild, key);
+
+    lbf = height(p->lchild) + 1;
+    rbf = height(p->rchild) + 1;
+    p->bf = lbf - rbf;
+
+    if (p->bf == 2 && p->lchild->bf == 1)
+        return LLRotation(p);
+    if (p->bf == 2 && p->lchild->bf == -1)
+        return LRRotation(p);
+    if (p->bf == -2 && p->rchild->bf == -1)
+        return RRRotation(p);
+    if (p->bf == -2 && p->rchild->bf == 1)
+        return RLRotation(p);
+
+    return p;
+}
+
+void Inorder(struct Node *p)
+{
+    if (p)
+    {
+        Inorder(p->lchild);
+        printf("%d ", p->data);
+        Inorder(p->rchild);
+    }
+}
+
+struct Node *Search(int key)
+{
+    struct Node *t = root;
+    while (t != NULL)
+    {
+        if (key == t->data)
+            return t;
+        else if (key < t->data)
+            t = t->lchild;
+        else
+            t = t->rchild;
+    }
+    return NULL;
 }
