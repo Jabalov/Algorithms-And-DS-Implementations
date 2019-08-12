ffb24734e2971cc8cfd6dbf57dd14d694d618bed
Jabalov <moh.aboajabl@gmail.com>
2019-08-12 20:47:56 +0200

    Insert

--- AVL_Trees/AVL.cpp
+++ AVL_Trees/AVL.cpp
@@ -19,3 +19,38 @@ int height(struct Node *p)
 
     return x > y ? x + 1 : y + 1;
 }
+
+void Insert(int key)
+{
+    struct Node *t = root;
+    struct Node *r = NULL, *p;
+
+    if (root == NULL)
+    {
+        p = (struct Node *)malloc(sizeof(struct Node));
+        p->data = key;
+        p->bf = 0;
+        p->lchild = p->rchild = NULL;
+        root = p;
+        return;
+    }
+    while (t != NULL)
+    {
+        r = t;
+        if (key < t->data)
+            t = t->lchild;
+        else if (key > t->data)
+            t = t->rchild;
+        else
+            return;
+    }
+
+    p = (struct Node *)malloc(sizeof(struct Node));
+    p->data = key;
+    p->lchild = p->rchild = NULL;
+
+    if (key < r->data)
+        r->lchild = p;
+    else
+        r->rchild = p;
+}
