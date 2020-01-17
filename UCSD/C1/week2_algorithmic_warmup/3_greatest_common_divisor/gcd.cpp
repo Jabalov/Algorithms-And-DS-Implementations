0bc332586a26b243b4060db674c1d7177191ae02
Muhammed Abu-Jabal <moh.aboajabl@gmail.com>
2020-01-17 23:20:25 +0200

    new

--- UCSD/C1/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.cpp
+++ UCSD/C1/week2_algorithmic_warmup/3_greatest_common_divisor/gcd.cpp
@@ -5,16 +5,11 @@ int gcd_naive(int a, int b)
 {
   int current_gcd = 1;
   for (int d = 2; d <= a && d <= b; d++)
-  {
     if (a % d == 0 && b % d == 0)
-    {
       if (d > current_gcd)
-      {
         current_gcd = d;
-      }
-    }
-  }
-  return current_gcd;
+
+    return current_gcd;
 }
 
 int gcd_fast(int a, int b)
