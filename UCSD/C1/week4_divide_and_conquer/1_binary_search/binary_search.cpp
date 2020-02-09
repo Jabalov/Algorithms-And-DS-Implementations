3863e61d3e2bd4cac9cd21721fc3dc62cacccd3f
jabalov <moh.aboajabl@gmail.com>
2020-02-09 21:50:25 +0200

    bs

--- UCSD/C1/week4_divide_and_conquer/1_binary_search/binary_search.cpp
+++ UCSD/C1/week4_divide_and_conquer/1_binary_search/binary_search.cpp
@@ -2,35 +2,52 @@
 #include <cassert>
 #include <vector>
 
-using std::vector;
+#define vi vector<int>
+using namespace std;
 
-int binary_search(const vector<int> &a, int x) {
-  int left = 0, right = (int)a.size(); 
-  //write your code here
-}
+int binary_search(const vi &a, int x)
+{
+  int left = 0, right = (int)a.size() - 1, mid;
+  while (left < right)
+  {
+    mid = (left + right) >> 1;
 
-int linear_search(const vector<int> &a, int x) {
-  for (size_t i = 0; i < a.size(); ++i) {
-    if (a[i] == x) return i;
+    if (a[mid] == x)
+      return mid;
+
+    else if (x < a[mid])
+      right = mid - 1;
+
+    else
+      left = mid + 1;
   }
+
   return -1;
 }
 
-int main() {
+int linear_search(const vi &a, int x)
+{
+  for (size_t i = 0; i < a.size(); ++i)
+    if (a[i] == x)
+      return i;
+
+  return -1;
+}
+
+int main()
+{
   int n;
-  std::cin >> n;
-  vector<int> a(n);
-  for (size_t i = 0; i < a.size(); i++) {
-    std::cin >> a[i];
-  }
+  cin >> n;
+  vi a(n);
+  for (size_t i = 0; i < a.size(); i++)
+    cin >> a[i];
+
   int m;
-  std::cin >> m;
-  vector<int> b(m);
-  for (int i = 0; i < m; ++i) {
-    std::cin >> b[i];
-  }
-  for (int i = 0; i < m; ++i) {
-    //replace with the call to binary_search when implemented
-    std::cout << linear_search(a, b[i]) << ' ';
-  }
+  cin >> m;
+  vi b(m);
+  for (int i = 0; i < m; ++i)
+    cin >> b[i];
+
+  for (int i = 0; i < m; ++i)
+    cout << binary_search(a, b[i]) << ' ';
 }
