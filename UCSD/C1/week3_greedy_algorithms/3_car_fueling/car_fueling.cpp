eb06a7ed0be051243d43e3afe8d5ebf4554e2231
jabalov <moh.aboajabl@gmail.com>
2020-02-09 16:15:22 +0200

    car fueling & dot_product

--- UCSD/C1/week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
+++ UCSD/C1/week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
@@ -1,18 +1,40 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
+#include <map>
 
-using std::cin;
-using std::cout;
-using std::vector;
-using std::max;
+using namespace std;
+#define vi vector<int>
+#define ll long long
 
-int compute_min_refills(int dist, int tank, vector<int> & stops) {
+int compute_min_refills(int dist, int tank, vector<int> &stops)
+{
     // write your code here
-    return -1;
+    ll cnt(0);
+    if (tank > dist)
+        return cnt;
+
+    if ((dist - stops[stops.size() - 1]) > tank)
+        return -1;
+
+    ll last_start = 0;
+    for (int i = 0; i < stops.size(); i++)
+    {
+        if (i && stops[i] - stops[i - 1] > tank)
+            return -1;
+
+        if (stops[i] - last_start > tank)
+            cnt++, last_start = stops[i - 1];
+    }
+
+    if (dist - last_start > tank)
+        cnt++;
+
+    return cnt;
 }
 
-
-int main() {
+int main()
+{
     int d = 0;
     cin >> d;
     int m = 0;
--- UCSD/C1/week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.cpp
+++ UCSD/C1/week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.cpp
@@ -1,27 +1,37 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
+#include <iomanip>
+#include <map>
+#include <algorithm>
 
+using namespace std;
 using std::vector;
+#define vi vector<int>
+#define ll long long
 
-long long max_dot_product(vector<int> a, vector<int> b) {
+ll max_dot_product(vi a, vi b)
+{
   // write your code here
-  long long result = 0;
-  for (size_t i = 0; i < a.size(); i++) {
-    result += ((long long) a[i]) * b[i];
-  }
+  sort(a.begin(), a.end());
+  sort(b.begin(), b.end());
+
+  ll result = 0;
+  for (size_t i = 0; i < a.size(); i++)
+    result += ((ll)a[i]) * (ll)b[i];
+
   return result;
 }
 
-int main() {
+int main()
+{
   size_t n;
-  std::cin >> n;
-  vector<int> a(n), b(n);
-  for (size_t i = 0; i < n; i++) {
-    std::cin >> a[i];
-  }
-  for (size_t i = 0; i < n; i++) {
-    std::cin >> b[i];
-  }
-  std::cout << max_dot_product(a, b) << std::endl;
+  cin >> n;
+  vi a(n), b(n);
+  for (size_t i = 0; i < n; i++)
+    cin >> a[i];
+
+  for (size_t i = 0; i < n; i++)
+    cin >> b[i];
+
+  cout << max_dot_product(a, b) << endl;
 }
