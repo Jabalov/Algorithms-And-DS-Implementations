cefd5123c5ccf8f54324c52d1c961164381a41e1
jabalov <moh.aboajabl@gmail.com>
2020-02-09 10:30:20 +0200

    fractional knapsack

--- UCSD/C1/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ UCSD/C1/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
+#include <map>
+
 using namespace std;
 #define vi vector<int>
 
 double get_optimal_value(int capacity, vi weights, vi values)
 {
   double value = 0.0;
+  int n = values.size();
+  multimap<double, long, greater<double>> valuePerUnit_and_index;
 
-  // write your code here
+  for (int i = 0; i < n; i++)
+    valuePerUnit_and_index.insert(make_pair((double)values[i] / (double)weights[i], i));
+
+  for (auto i = valuePerUnit_and_index.begin(); i != valuePerUnit_and_index.end(); i++)
+  {
+    if (capacity == 0)
+      return value;
+
+    double a = min(capacity, weights[i->second]);
+    capacity -= a;
+    value += a * (double)i->first;
+  }
 
   return value;
 }
@@ -18,8 +33,10 @@ int main()
   int n;
   int capacity;
   cin >> n >> capacity;
+
   vi values(n);
   vi weights(n);
+
   for (int i = 0; i < n; i++)
     cin >> values[i] >> weights[i];
 
