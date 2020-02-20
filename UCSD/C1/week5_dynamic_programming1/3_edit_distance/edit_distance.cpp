2c61c424c750bbe8fce6ed72b15fba132858eccc
jabalov <moh.aboajabl@gmail.com>
2020-02-20 09:23:26 +0200

    alignment game

--- UCSD/C1/week5_dynamic_programming1/3_edit_distance/edit_distance.cpp
+++ UCSD/C1/week5_dynamic_programming1/3_edit_distance/edit_distance.cpp
@@ -5,14 +5,35 @@
 #include <iostream>
 #include <algorithm>
 #include <cstdlib>
+#include <string>
 
 #define vi vector<int>
+#define vii vector<vector<int>>
 #define ll long long
 using namespace std;
 
 int edit_distance(const string &str1, const string &str2)
 {
-  return 0;
+  int len_str1 = str1.length(), len_str2 = str2.length();
+  vii dist((len_str1 + 1), vi(len_str2 + 1));
+
+  for (int i = 0; i <= len_str1; i++)
+    for (int j = 0; j <= len_str2; j++)
+    {
+      if (i == 0)
+        dist[i][j] = j;
+
+      else if (j == 0)
+        dist[i][j] = i;
+
+      else if (str1[i - 1] == str2[j - 1])
+        dist[i][j] = dist[i - 1][j - 1];
+
+      else
+        dist[i][j] = 1 + min(min(dist[i][j - 1], dist[i - 1][j]), dist[i - 1][j - 1]);
+    }
+
+  return dist[len_str1][len_str2];
 }
 
 int main()
