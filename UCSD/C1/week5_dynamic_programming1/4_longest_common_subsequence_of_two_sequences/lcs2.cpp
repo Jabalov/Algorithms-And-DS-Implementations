2b2c057a1bbf7070dc0dcb10fa35cd29b501f03e
jabalov <moh.aboajabl@gmail.com>
2020-02-20 10:18:44 +0200

    lcs2 and lcs3

--- UCSD/C1/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.cpp
+++ UCSD/C1/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.cpp
@@ -1,27 +1,51 @@
 #include <iostream>
+#include <cassert>
 #include <vector>
+#include <map>
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include <string>
 
-using std::vector;
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define ll long long
+using namespace std;
 
-int lcs2(vector<int> &a, vector<int> &b) {
-  //write your code here
-  return std::min(std::min(a.size(), b.size()), c.size());
+int lcs2(vi &a, vi &b)
+{
+  int m = a.size(), n = b.size();
+  vii A(m + 1, vi(n + 1));
+
+  for (int i = 0; i <= m; i++)
+    for (int j = 0; j <= n; j++)
+    {
+      if (i == 0 || j == 0)
+        A[i][j] = 0;
+
+      else if (a[i - 1] == b[j - 1])
+        A[i][j] = A[i - 1][j - 1] + 1;
+
+      else
+        A[i][j] = max(A[i - 1][j], A[i][j - 1]);
+    }
+
+  return A[m][n];
 }
 
-int main() {
+int main()
+{
   size_t n;
-  std::cin >> n;
-  vector<int> a(n);
-  for (size_t i = 0; i < n; i++) {
-    std::cin >> a[i];
-  }
+  cin >> n;
+  vi a(n);
+  for (size_t i = 0; i < n; i++)
+    cin >> a[i];
 
   size_t m;
-  std::cin >> m;
-  vector<int> b(m);
-  for (size_t i = 0; i < m; i++) {
-    std::cin >> b[i];
-  }
+  cin >> m;
+  vi b(m);
+  for (size_t i = 0; i < m; i++)
+    cin >> b[i];
 
-  std::cout << lcs2(a, b) << std::endl;
+  cout << lcs2(a, b) << endl;
 }
--- UCSD/C1/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.cpp
+++ UCSD/C1/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.cpp
@@ -1,31 +1,67 @@
 #include <iostream>
+#include <cassert>
 #include <vector>
+#include <map>
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include <string>
 
-using std::vector;
+#define vi vector<int>
+#define vii vector<vector<int>>
+#define viii vector<vector<vector<int>>>
+#define ll long long
+using namespace std;
 
-int lcs3(vector<int> &a, vector<int> &b, vector<int> &c) {
-  //write your code here
-  return std::min(std::min(a.size(), b.size()), c.size());
+int lcs3(vi &a, vi &b, vi &c)
+{
+  int m = a.size();
+  int n = b.size();
+  int z = c.size();
+
+  viii A(m + 1, vii(n + 1, vi(z + 1)));
+
+  for (int i = 0; i <= m; i++)
+    for (int j = 0; j <= n; j++)
+      for (int k = 0; k <= z; k++)
+        if (i == 0 || j == 0 || k == 0)
+        {
+          A[i][j][k] = 0;
+          continue;
+        }
+
+        else if (a[i - 1] == b[j - 1] && a[i - 1] == c[k - 1])
+        {
+          A[i][j][k] = A[i - 1][j - 1][k - 1] + 1;
+          continue;
+        }
+        else
+        {
+          A[i][j][k] = max(max(A[i - 1][j][k], A[i][j - 1][k]), A[i][j][k - 1]);
+        }
+
+  return A[m][n][z];
 }
 
-int main() {
+int main()
+{
   size_t an;
-  std::cin >> an;
-  vector<int> a(an);
-  for (size_t i = 0; i < an; i++) {
-    std::cin >> a[i];
-  }
+  cin >> an;
+  vi a(an);
+  for (size_t i = 0; i < an; i++)
+    cin >> a[i];
+
   size_t bn;
-  std::cin >> bn;
-  vector<int> b(bn);
-  for (size_t i = 0; i < bn; i++) {
-    std::cin >> b[i];
-  }
+  cin >> bn;
+  vi b(bn);
+  for (size_t i = 0; i < bn; i++)
+    cin >> b[i];
+
   size_t cn;
-  std::cin >> cn;
-  vector<int> c(cn);
-  for (size_t i = 0; i < cn; i++) {
-    std::cin >> c[i];
-  }
-  std::cout << lcs3(a, b, c) << std::endl;
+  cin >> cn;
+  vi c(cn);
+  for (size_t i = 0; i < cn; i++)
+    cin >> c[i];
+
+  cout << lcs3(a, b, c) << endl;
 }
