8840502e51423b540926b055f519c9b76af1ef35
jabalov <moh.aboajabl@gmail.com>
2020-02-24 22:39:48 +0200

    slight edit

--- UCSD/C1/week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.cpp
+++ UCSD/C1/week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.cpp
@@ -18,21 +18,16 @@ using namespace std;
 ll eval(ll a, ll b, char op)
 {
   if (op == '*')
-  {
     return a * b;
-  }
+
   else if (op == '+')
-  {
     return a + b;
-  }
+
   else if (op == '-')
-  {
     return a - b;
-  }
+
   else
-  {
     assert(0);
-  }
 }
 
 ll get_maximum_value(const string &exp)
@@ -40,14 +35,14 @@ ll get_maximum_value(const string &exp)
   int len = exp.size();
   int num = (len + 1) / 2;
 
-  long long mini[num][num];
-  long long maxi[num][num];
+  ll mini[num][num];
+  ll maxi[num][num];
 
   memset(mini, 0, sizeof(mini));
   memset(maxi, 0, sizeof(maxi));
+
   for (int i = 0; i < num; i++)
   {
-
     mini[i][i] = stoll(exp.substr(2 * i, 1));
     maxi[i][i] = stoll(exp.substr(2 * i, 1));
   }
@@ -56,15 +51,15 @@ ll get_maximum_value(const string &exp)
     for (int i = 0; i < num - s - 1; i++)
     {
       int j = i + s + 1;
-      long long miniValue = LLONG_MAX;
-      long long maxiValue = LLONG_MIN;
+      ll miniValue = LLONG_MAX;
+      ll maxiValue = LLONG_MIN;
 
       for (int k = i; k < j; k++)
       {
-        long long a = eval(mini[i][k], mini[k + 1][j], exp[2 * k + 1]);
-        long long b = eval(mini[i][k], maxi[k + 1][j], exp[2 * k + 1]);
-        long long c = eval(maxi[i][k], mini[k + 1][j], exp[2 * k + 1]);
-        long long d = eval(maxi[i][k], maxi[k + 1][j], exp[2 * k + 1]);
+        ll a = eval(mini[i][k], mini[k + 1][j], exp[2 * k + 1]);
+        ll b = eval(mini[i][k], maxi[k + 1][j], exp[2 * k + 1]);
+        ll c = eval(maxi[i][k], mini[k + 1][j], exp[2 * k + 1]);
+        ll d = eval(maxi[i][k], maxi[k + 1][j], exp[2 * k + 1]);
 
         miniValue = min(miniValue, min(a, min(b, min(c, d))));
         maxiValue = max(maxiValue, max(a, max(b, max(c, d))));
