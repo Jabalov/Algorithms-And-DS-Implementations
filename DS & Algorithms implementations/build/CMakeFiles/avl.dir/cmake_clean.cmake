8901d1c51310fbe3a2cdecec0660c0ac6403c490
jabalov <moh.aboajabl@gmail.com>
2020-04-02 12:16:51 +0200

    compile has lost his mind -_-

---
+++ DS & Algorithms implementations/build/CMakeFiles/avl.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/avl.dir/main.cpp.obj"
+  "avl.exe"
+  "avl.exe.manifest"
+  "avl.pdb"
+  "libavl.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/avl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ DS & Algorithms implementations/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/user/Desktop/Self-Study/Stack-Queue/DS & Algorithms implementations
+# Build directory: C:/Users/user/Desktop/Self-Study/Stack-Queue/DS & Algorithms implementations/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
