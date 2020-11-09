d9d46e7f18fe18e43c74393aefe7f539a853d741
Your Name <moh.aboajabl@gmail.com>
2020-11-09 16:19:50 +0200

    init Hashtable

---
+++ HashTable/HashTableLinearProbing.h
@@ -0,0 +1,10 @@
+/*
+ * @file   HashTableLinearProbing.h
+ * @author (original JAVA) William Fiset, william.alexandre.fiset@gmail.com
+ *         (into C++) Jabalov
+ */
+
+#ifndef D_HASHTABLELINEARPROBING_H
+#define D_HASHTABLELINEARPROBING_H
+
+#endif
