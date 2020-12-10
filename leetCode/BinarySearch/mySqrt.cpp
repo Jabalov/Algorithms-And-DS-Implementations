e04cd78d4396e7ede04207b4a97f80c7fda4d269
jabalov <moh.aboajabl@gmail.com>
2020-12-10 18:11:31 +0200

    mySqrt

---
+++ leetCode/BinarySearch/mySqrt.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <iterator>
+#include <numeric>
+#include <algorithm>
+#include <string.h>
+#include <assert.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int mySqrt(int x) 
+    {
+        int start = 1, end = x;
+        int ans;
+        if (x == 0)
+          return 0;
+
+        while(start <= end)
+        {
+            int mid = (start + end) >> 1;
+
+            if(mid == x / mid)
+                return mid;
+          
+            else if(mid < x / mid)
+            {
+                ans = mid;
+                start = mid + 1;
+            }
+            else
+                end = mid - 1;
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    auto sol = Solution();
+    vector<int> v = {4, 5, 6, 7, 0, 1, 2};
+    int s = sol.mySqrt(10);
+    cout << s << " ";
+}
