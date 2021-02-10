22c51fa9e0a3722d980467713b7caee13ecba485
jabalov <moh.aboajabl@gmail.com>
2021-02-10 20:54:02 +0200

    sdd

---
+++ leetCode/Trees/countNodes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <iterator>
+#include <numeric>
+#include <algorithm>
+#include <string>
+#include <map>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+
+        return countNodes(root->left) + countNodes(root->right) + 1;
+    }
+};
