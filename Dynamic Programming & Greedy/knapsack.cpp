7c6805e0a12b08572e958497541b0af1fd904b17
jabalov <moh.aboajabl@gmail.com>
2021-04-20 21:32:37 +0200

    ks memo

--- Dynamic Programming & Greedy/knapsack.cpp
+++ Dynamic Programming & Greedy/knapsack.cpp
@@ -43,9 +43,30 @@ int kp_memo(int wt[], int profit[], int w, int n)
     return memo[w][n];
 }
 
+int knapsack(int W, int wt[], int profit[], int n)
+{
+    int i, w;
+    int dp[n + 1][W + 1];
+ 
+    for(i = 0; i <= n; i++)
+        for(w = 0; w <= W; w++)
+        {
+            if (i == 0 || w == 0)
+                dp[i][w] = 0;
+
+            else if (wt[i - 1] <= w)
+                dp[i][w] = max(profit[i - 1] + dp[i - 1][w - wt[i - 1]],
+                                dp[i - 1][w]);
+            else
+                dp[i][w] = dp[i - 1][w];
+        }
+    
+    return dp[n][W];
+}
+
 int main()
 {
     int wt[3] = {3, 2, 4};
     int profit[3] = {6, 8, 7};
-    cout << kp_memo(wt, profit, 8, 3);
+    cout << knapsack(8, wt, profit, 3);
 }
