36cc22cb473bd1b1a5a7cd0857378b345c67f489
jabalov <moh.aboajabl@gmail.com>
2021-04-25 19:27:42 +0200

    add

---
+++ leetCode/Dynamic Programming/coinChange2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <iterator>
+#include <numeric>
+#include <algorithm>
+#include <string.h>
+#include <assert.h>
+using namespace std;
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1));
+
+        for (int i = 1; i <= amount; i++)
+            dp[0][i] = 0;
+
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = 1;
+
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= amount; j++)
+            {
+                if(j >= coins[i - 1])
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+
+        return dp[n][amount];
+    }
+};
